9e551d7d316cc9b11ab038eef041de988b73a92f
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-06 14:38:48 -0300

    container and header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Container from '@/components/Container';
+import Header from '@/components/Header';
 import PostList from '@/components/PostList';
 import { SpinLoader } from '@/components/SpinLoader';
 import React, { Suspense } from 'react';
@@ -6,16 +7,7 @@ import React, { Suspense } from 'react';
 export default async function HomePage() {
   return (
     <Container>
-      <header>
-        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a header</h1>
-      </header>
-
-      <p className='text-justify'>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
-        quibusdam quaerat, suscipit, tempore harum impedit magni sunt illum
-        nesciunt similique veniam reiciendis ullam necessitatibus sequi
-        repudiandae doloribus! Eum, natus rerum.
-      </p>
+      <Header />
 
       <Suspense fallback={<SpinLoader />}>
         <PostList />
--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -7,9 +7,7 @@ type ContainerProps = {
 export default function Container({ children }: ContainerProps) {
   return (
     <div className='text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100'>
-      <div className='bg-amber-500 max-w-screen-lg mx-auto px-8'>
-        {children}
-      </div>
+      <div className=' max-w-screen-lg mx-auto px-8'>{children}</div>
     </div>
   );
 }
