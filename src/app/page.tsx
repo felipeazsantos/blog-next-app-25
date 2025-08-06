d6a49b9278a62fec570c2fad5070773bf8776ec1
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-06 14:28:03 -0300

    creating container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
+import Container from '@/components/Container';
 import PostList from '@/components/PostList';
 import { SpinLoader } from '@/components/SpinLoader';
 import React, { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <div>
+    <Container>
       <header>
         <h1 className='text-6xl font-bold text-center py-8'>Aqui é a header</h1>
       </header>
+
+      <p className='text-justify'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda
+        quibusdam quaerat, suscipit, tempore harum impedit magni sunt illum
+        nesciunt similique veniam reiciendis ullam necessitatibus sequi
+        repudiandae doloribus! Eum, natus rerum.
+      </p>
 
       <Suspense fallback={<SpinLoader />}>
         <PostList />
@@ -16,6 +24,6 @@ export default async function HomePage() {
       <footer>
         <p className='text-6xl font-bold text-center py-8'>Aqui é a footer</p>
       </footer>
-    </div>
+    </Container>
   );
 }
---
+++ src/components/Container/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+export default function Container({ children }: ContainerProps) {
+  return (
+    <div className='text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100'>
+      <div className='bg-amber-500 max-w-screen-lg mx-auto px-8'>
+        {children}
+      </div>
+    </div>
+  );
+}
