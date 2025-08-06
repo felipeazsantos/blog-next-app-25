138ec874964712c4fff7cb40db995c398610cd9e
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-06 14:47:00 -0300

    building featurePost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,32 @@ import Container from '@/components/Container';
 import Header from '@/components/Header';
 import PostList from '@/components/PostList';
 import { SpinLoader } from '@/components/SpinLoader';
+import Image from 'next/image';
+import Link from 'next/link';
 import React, { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-xl' href={'#'}>
+          <Image
+            className='group-hover:scale-105 transition'
+            src={'/images/bryen_0.png'}
+            height={1200}
+            width={720}
+            alt='Título do post'
+          />
+        </Link>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum sed
+          vel nulla consequuntur maiores, pariatur necessitatibus. Accusantium
+          porro accusamus labore earum officiis, quidem, iste temporibus iure
+          tempora repellat placeat recusandae!
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostList />
