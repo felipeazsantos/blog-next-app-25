229e10a327810ffefcbe82d346eae94280de173d
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-07 11:52:26 -0300

    organizing layout and homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,15 @@
-import Container from '@/components/Container';
-import Header from '@/components/Header';
 import PostFeatured from '@/components/PostFeatured';
 import PostList from '@/components/PostList';
 import { SpinLoader } from '@/components/SpinLoader';
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
+    <div className='min-h-screen'>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
-      </Suspense>
-
-      <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
-
-      <footer>
-        <p className='text-6xl font-bold text-center py-8'>Aqui é a footer</p>
-      </footer>
-    </Container>
+    </div>
   );
 }
