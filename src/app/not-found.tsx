b7cdd38ed90319671e9bdaf6319f9ad600392feb
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-07 12:04:46 -0300

    creating NotFound page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,26 @@
+import clsx from 'clsx';
+import React from 'react';
+
+export default function NotFoundPage() {
+  return (
+    <div
+      className={clsx(
+        'min-h-[320px]',
+        'bg-slate-900',
+        'text-slate-100',
+        'mb-16',
+        'p-8',
+        'rounded-xl',
+        'flex',
+        'flex-col',
+        'items-center',
+        'justify-center',
+      )}
+    >
+      <h1 className='text-7xl/tight mb-4 font-extrabold'>404</h1>
+      <p>
+        Erro 404 = A página que você está tentando acessar não existe nesse site
+      </p>
+    </div>
+  );
+}
