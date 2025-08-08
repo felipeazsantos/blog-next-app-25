a6787c3a6b6ec5a52c4ca1d8098a97bcd4cceb70
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-08 11:49:14 -0300

    installing tailwind typography plugin and styling post

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React from 'react';
 import ReactMarkDown from 'react-markdown';
 import rehypeSanitize from 'rehype-sanitize';
@@ -9,7 +10,21 @@ type SafeMarkdownProps = {
 
 export default function SafeMarkdown({ markdown }: SafeMarkdownProps) {
   return (
-    <div>
+    <div
+      className={clsx(
+        'prose prose-slate',
+        'w-full',
+        'max-w-none',
+        'overflow-hidden',
+        'prose-a:transition',
+        'prose-a:no-underline',
+        'prose-a:text-blue-500',
+        'prose-a:hover:text-blue-700',
+        'prose-a:hover:underline',
+        'prose-img:mx-auto',
+        'md:prose-lg',
+      )}
+    >
       <ReactMarkDown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}
