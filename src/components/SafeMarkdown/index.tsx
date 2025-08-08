5a4beaf5d3087130adc9f9cfb54449efddd5821d
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-08 11:56:14 -0300

    improving table ux

--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -28,6 +28,17 @@ export default function SafeMarkdown({ markdown }: SafeMarkdownProps) {
       <ReactMarkDown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ node, ...props }) => {
+            if (!node?.children) return '';
+
+            return (
+              <div className='overflow-x-auto'>
+                <table className='w-full min-w-[600px]' {...props} />
+              </div>
+            );
+          },
+        }}
       >
         {markdown}
       </ReactMarkDown>
