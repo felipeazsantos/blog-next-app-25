a024d23894cddb025f343117383c02e460f73d2b
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-29 13:00:57 -0300

    open close menu

--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,24 +1,67 @@
+'use client';
+
 import clsx from 'clsx';
-import { FileTextIcon, HouseIcon } from 'lucide-react';
+import {
+  CircleXIcon,
+  FileTextIcon,
+  HouseIcon,
+  MenuIcon,
+  PlusIcon,
+} from 'lucide-react';
 import Link from 'next/link';
-import React from 'react';
+import { usePathname } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
 export default function MenuAdmin() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
   const navClasses = clsx(
     'bg-slate-900 text-slate-100 rounded-lg overflow-hidden',
     'flex flex-col mb-8 sm:flex-row sm:flex-wrap',
-    'h-10',
+    !isOpen && 'h-10',
+    'sm:overflow-visible sm:h-auto',
   );
+
   const linkClasses = clsx(
     '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4',
     'flex items-center gap-2',
     'transition hover:bg-slate-800',
     'h-10',
     'shrink-0',
+    'cursor-pointer',
+  );
+
+  const openCloseBtnClasses = clsx(
+    linkClasses,
+    'text-blue-200 italic',
+    'sm:hidden',
   );
 
   return (
     <nav className={navClasses}>
+      <button
+        onClick={() => setIsOpen(open => !open)}
+        className={openCloseBtnClasses}
+      >
+        {!isOpen && (
+          <>
+            <MenuIcon />
+            Menu
+          </>
+        )}
+
+        {isOpen && (
+          <>
+            <CircleXIcon />
+            Fechar
+          </>
+        )}
+      </button>
       <a className={linkClasses} href='/' target='_blank'>
         <HouseIcon />
         Home
@@ -27,6 +70,11 @@ export default function MenuAdmin() {
       <Link className={linkClasses} href={'/admin/post'}>
         <FileTextIcon />
         Posts
+      </Link>
+
+      <Link className={linkClasses} href={'/admin/post/new'}>
+        <PlusIcon />
+        Criar post
       </Link>
     </nav>
   );
