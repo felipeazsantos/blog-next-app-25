be36d9af750924d82c305742c887d4f7ff3ebdce
felipeazsantos <felipeasantos10@hotmail.com>
2025-09-03 19:57:00 -0300

    markdown editor component

--- src/components/admin/ManagedPostForm/index.tsx
+++ src/components/admin/ManagedPostForm/index.tsx
@@ -3,14 +3,25 @@
 import Button from '@/components/Button';
 import InputCheckbox from '@/components/InputCheckbox';
 import InputText from '@/components/InputText';
-import React from 'react';
+import MarkdownEditor from '@/components/MarkdownEditor';
+import React, { useState } from 'react';
 
 export default function ManagedPostForm() {
+  const [value, setValue] = useState('');
+
   return (
     <form action='' className='mb-16'>
       <div className='flex flex-col gap-6'>
         <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
         <InputCheckbox labelText='Sobrenome' />
+
+        <MarkdownEditor
+          labelText='Conteúdo'
+          value={value}
+          setValue={setValue}
+          textAreaName='content'
+          disabled={false}
+        />
       </div>
 
       <div className='mt-4'>
