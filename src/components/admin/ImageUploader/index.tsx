abcdab856d32e32c93b18754ac6178a1d1c2a7ee
felipeazsantos <felipeasantos10@hotmail.com>
2025-09-05 12:41:51 -0300

    catch image to send to the server

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import Button from '@/components/Button';
+import { IMAGE_UPLOADER_MAX_SIZE } from '@/lib/constants';
 import { ImageUpIcon } from 'lucide-react';
 import { useRef } from 'react';
+import { toast } from 'react-toastify';
 
 const ImageUploader = () => {
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -11,6 +13,28 @@ const ImageUploader = () => {
     if (!fileInputRef.current) return;
 
     fileInputRef.current.click();
+  }
+
+  function handleChange() {
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.item(0);
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOADER_MAX_SIZE) {
+      const readableMaxSize = IMAGE_UPLOADER_MAX_SIZE / 1024;
+      toast.error(`imagem muito grande. Max.: ${readableMaxSize}KB.`);
+
+      fileInput.value = '';
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    fileInput.value = '';
   }
 
   return (
@@ -22,6 +46,7 @@ const ImageUploader = () => {
 
       <input
         ref={fileInputRef}
+        onChange={handleChange}
         className='hidden'
         type='file'
         name='file'
